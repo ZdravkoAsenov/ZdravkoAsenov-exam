f5ee6f4c23821bb9684945d43e872a5369c2bab3
ZdravkoAsenov <zdravkoasenov2004@abv.bg>
2023-12-16 14:35:25 +0200

    Fix the wrong UI test

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('https://zdravkoasenov-exam.onrender.com/');
+    await page.goto('https://zdravkoasenov-exam.onrender.com/Shopping-List');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
