6f7cdad7f7962108b9e13378472effc91d8a4c63
ZdravkoAsenov <zdravkoasenov2004@abv.bg>
2023-12-16 14:30:32 +0200

    Fix ui test url

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/shopping-list');
+    await page.goto('https://zdravkoasenov-exam.onrender.com/');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
